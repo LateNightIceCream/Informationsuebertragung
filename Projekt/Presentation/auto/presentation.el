6e59a84fd8393fdf47b3072be0f917588324d669
LateNightIceCream <richardgruenert@live.com>
2020-06-10 22:58:41 +0200

    presentation update

--- Projekt/Presentation/auto/presentation.el
+++ Projekt/Presentation/auto/presentation.el
@@ -2,8 +2,11 @@
  "presentation"
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-class-options
-                     '(("beamer" "aspectratio=1610")))
+                     '(("beamer" "aspectratio=1610" "xcolor=table")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("siunitx" "binary-units")))
    (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
@@ -11,20 +14,33 @@
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
    (TeX-run-style-hooks
     "latex2e"
     "colors"
+    "slides/Aufgabe1"
+    "slides/Aufgabe2"
+    "slides/Aufgabe3"
+    "slides/Aufgabe4"
     "beamer"
     "beamer10"
     "graphicx"
     "xcolor"
     "listings"
     "bera"
+    "siunitx"
     "fontspec"
     "tgheros"
     "inconsolata")
-   (LaTeX-add-xcolor-definecolors
-    "keywords"))
+   (TeX-add-symbols
+    '("twocolumns" 3)
+    "smallemskip"
+    "medemskip"
+    "bigemskip"
+    "smallgrskip"
+    "medgrskip"
+    "biggrsikip"))
  :latex)
 
